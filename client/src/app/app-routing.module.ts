f43cd253dbe984b2bf66d57c05dbb1edc220d698
Siarhei Karpovich1 <skarpovich@cloudpassage.com>
2018-11-01 16:01:35 +0300

    Added login page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { AuthLayoutsComponent } from './shared/layouts/auth-layouts/auth-layouts.component';
+import { SiteLayoutsComponent } from './shared/layouts/site-layouts/site-layouts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthLayoutsComponent,
+    children: [
+      { path: '', redirectTo: '/login', pathMatch: 'full' }
+      { path: 'login', component: LoginPageComponent },
+      { path: 'register', component: RegisterPageComponent }
+    ]
+  },
+  {
+      path: '',
+      component: SiteLayoutsComponent,
+      children: []
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthLayoutsComponent } from './shared/layouts/auth-layouts/auth-layouts.component';
+import { SiteLayoutsComponent } from './shared/layouts/site-layouts/site-layouts.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    AuthLayoutsComponent,
+    SiteLayoutsComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
