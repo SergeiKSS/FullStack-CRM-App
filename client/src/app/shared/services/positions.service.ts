1d8512a2ae0f9b8da8c04af1208dbfbba0020208
Sergei <SergeiKSS@mail.ru>
2018-11-18 01:43:30 +0300

    Added order positions

--- client/src/app/shared/services/positions.service.ts
+++ client/src/app/shared/services/positions.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
-import { Position } from '../interfaces'
+import { Position, Message } from '../interfaces'
 
 @Injectable({
   providedIn: 'root'
