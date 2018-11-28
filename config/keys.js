65d7f2448d7da76f5f3f90aa2fad9d1989b85417
Siarhei Karpovich1 <skarpovich@cloudpassage.com>
2018-11-28 09:39:13 +0300

    Finish

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://sergei:123456a@ds143293.mlab.com:43293/fullstack-crm-app',
-  jwt: 'dev-jwt'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys.prod')
+} else {
+  module.exports = require('./keys.dev')
 }
